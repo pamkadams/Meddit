fea133c1ff9dc20db154a46e2ba5b3b9a7a23e14
Pam Adams <pkadams@epotential.com>
2019-10-16 15:18:36 -0400

    article data extracted

--- js/app.js
+++ js/app.js
@@ -5,12 +5,7 @@ $(() => {
 
   //extracts key data from metadata record for article
   const gotMetaData = metadata => {
-    //article is an Object keys for display via the DOM:
-    //all fields are off these paths -- result.articleid. (both are objects that hold the rest)
-    //FYI articleId  is in the object articleids which is an array and is id is in index 2 -  example - 2: {idtype: "pmcid", value: "PMC6791746"} from the pmc query is the pmcid ID number (actually a string). The initial query in this app does NOT have PMC in front of the id.
-    //authors[0] - array and first author of the paper is index 0
-    //pubdate - publication date mm/dd/yy string
-    //fulljournalname - journal publication
+    //The article id is buried in the json object, it is pulled out below as $uids
     const $uids = metadata.result.uids[0];
 
     const articleObj = {
@@ -23,7 +18,8 @@ $(() => {
 
     console.log(articleObj);
     articleArr.push(articleObj);
-    displayArticle(articleObj);
+    //pass the article object to the display function
+    //displayArticle(articleObj);
   };
 
   //use article id to grab metadata record for each article
