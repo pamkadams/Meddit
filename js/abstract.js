20163a91870196e527e6e7b8af97c16e6989dd47
Pam Adams <pkadams@epotential.com>
2019-10-21 17:31:35 -0400

    spacing

--- js/abstract.js
+++ js/abstract.js
@@ -42,3 +42,11 @@ $(".results").on("click", "#nextbtn", event => {
 
 const $findInLib = $("<p>");
 $($articleContainer.append('<button>Find in Library</button>')
+
+
+// // $(".container").on("click", worldCat);
+// $(".allarticles").on("click", ".library", event => {
+//   console.log("working");
+//   // currentPage = [];
+//   // $(".allarticles").html("");
+//});
---
+++ js/worldcat.js
@@ -0,0 +1,10 @@
+const $findInLib = $("<p>");
+$($articleContainer.append('<button>Find in Library</button>')
+
+
+$(".container").on("click", worldCat);
+ $(".allarticles").on("click", ".library", event => {
+ console.log("working");
+currentPage = [];
+ $(".allarticles").html("");
+//});
